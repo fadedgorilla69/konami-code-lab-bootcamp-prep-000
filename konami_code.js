1b125a700a47c26a58784a10bebdae9e93a76634
Phillip Green <phillycheez11@gmail.com>
2018-06-16 18:04:47 +0000

    automatically backed up by learn

---
+++ konami_code.js
@@ -0,0 +1,18 @@
+const codes = [
+  "ArrowUp",
+  "ArrowUp",
+  "ArrowDown",
+  "ArrowDown",
+  "ArrowLeft",
+  "ArrowRight",
+  "ArrowLeft",
+  "ArrowRight",
+  "b",
+  "a"
+];
+ 
+function init() {
+  
+  
+  
+}
