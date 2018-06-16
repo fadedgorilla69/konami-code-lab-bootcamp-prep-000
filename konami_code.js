0fc3a44d362774d136d9485f283fe9ba1628d310
Phillip Green <phillycheez11@gmail.com>
2018-06-16 18:02:31 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,27 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+
+
+
 
 function init() {
-  // Write your JavaScript code inside the init() function
+// const alphabet = [65, 66, 67];
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+var index = 0;
+var placement = 0;
+  document.body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.which || e.detail);
+    
+    if (placement < 1000){console.log(e.code);
+    
+    if (code[index] === key) {
+      index++;
 
+      if (index === code.length) {
+        alert('Hurray!');
+
+        index = 0;
+      }
+    }} else {
+      index = 0;
+    }
+  });
 }
